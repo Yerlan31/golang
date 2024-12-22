f02f4b279a6643a1a3b6016bd118406067abe997
“ruamhh” <mukhanov-yerzhan@mail.ru>
2024-12-22 14:08:42 +0300

    basic declaration

--- 2/99_hw/signer/signer.go
+++ 2/99_hw/signer/signer.go
@@ -1,3 +1,31 @@
 package main
 
-// сюда писать код
+import (
+	"fmt"
+)
+
+func ExecutePipeline(jobsArray ... job){
+	fmt.Println("ExecutePipeline!")
+	in := make(chan interface{})
+    out := make(chan interface{})
+	for _ , oneJob := range jobsArray {
+		go oneJob(in,out)
+	}
+}
+
+func SingleHash(in, out chan interface{}){
+	fmt.Println("SingleHash!")
+}
+
+func MultiHash(in, out chan interface{}){
+	fmt.Println("MultiHash!")
+}
+
+func CombineResults(in, out chan interface{}){
+	fmt.Println("CombineResults!")
+}
+
+func main(){
+	fmt.Println("Hello!")
+	// сюда писать код
+}
